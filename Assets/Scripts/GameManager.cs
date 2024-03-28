0850cfd40a3a7a6e3c115066a767f75ffca49e9b
Enes Bahtiyar <97892255+enesbahtiyar@users.noreply.github.com>
2024-03-28 18:56:58 +0300

    oyun dosyasını githuba atmaca

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,125 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    //burası spawn kısmı 
+    [SerializeField] List<Transform> spawns = new List<Transform>();
+    [SerializeField] List<Transform> spawnsWalk = new List<Transform>();
+    [SerializeField] List<Transform> spawnsTurret = new List<Transform>();
+    int randSpawn;
+
+
+    //bu kısım player kontrol kısmı
+    //yazı elemanına bir referans verelim
+    [SerializeField] public TMP_Text playersText;
+    //oyuncuları depolayacak liste ya da array
+    GameObject[] players;
+    //aktif oyuncuların isimlerini depolayacak bir liste
+    List<string> activePlayers = new List<string>();
+
+    int checkPlayers = 0;
+    int previousPlayerCount;
+
+    void Start()
+    {        
+        randSpawn = Random.Range(0, spawns.Count);
+        PhotonNetwork.Instantiate("Player", spawns[randSpawn].position, spawns[randSpawn].rotation);
+        Invoke("SpawnEnemy", 5f);
+        previousPlayerCount = PhotonNetwork.PlayerList.Length;
+    }
+
+    private void Update()
+    {
+        if(PhotonNetwork.PlayerList.Length < previousPlayerCount)
+        {
+            ChangePlayerList();
+        }
+        previousPlayerCount = PhotonNetwork.PlayerList.Length;
+    }
+
+
+    public void SpawnEnemy()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            for (int i = 0; i < spawnsWalk.Count; i++)
+            {
+                PhotonNetwork.Instantiate("WalkEnemy", spawnsWalk[i].position, spawnsWalk[i].rotation);
+            }
+            for (int i = 0; i < spawnsTurret.Count; i++)
+            {
+                PhotonNetwork.Instantiate("Turret", spawnsTurret[i].position, spawnsTurret[i].rotation);
+            }
+        }
+    }
+
+    public void ChangePlayerList()
+    {
+        photonView.RPC("PlayerList", RpcTarget.All);
+    }
+
+    [PunRPC]
+    public void PlayerList()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        activePlayers.Clear();
+
+        //aktif oyuncular listesini oluştur
+        //bi tane listenin içerisindeki bütün elemanlar için şunları şunları yap
+        foreach(GameObject player in players)
+        {
+            //eğer oyuncu hayattaysa
+            if(player.GetComponent<PlayerController>().dead == false)
+            {
+                //aktif oyuncular içerisine ismini ekle
+                activePlayers.Add(player.GetComponent<PhotonView>().Owner.NickName);
+            }
+        }
+
+        //eğer oyunda 1 kişi kaldıysa yani kazanan kişi belli olduysa düşmanları durdur
+        if(activePlayers.Count <= 1 && checkPlayers > 0)
+        {
+            PlayerPrefs.SetString("winner", activePlayers[0]);
+            //oyundaki bütün düşmanları bul 
+            var enemies = GameObject.FindGameObjectsWithTag("enemy");
+            //var variable demek variable da değişken demek
+            //listedeki düşmanlrın hepsini öldür
+            foreach(GameObject enemy in enemies)
+            {
+                enemy.GetComponent<Enemy>().ChangeHealth(999999);
+            }
+            Invoke("EndGame", 5f);
+        }
+
+        checkPlayers++;
+
+
+        //bişey olana kadar bişeyler yap
+        //for(int i = 0; i > 10; i++)
+    }
+
+    void EndGame()
+    {
+        //lobiye geri dön
+        PhotonNetwork.LoadLevel("Lobby");
+    }
+
+    public void ExitGame()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        //odadan ayrılan kişiyi menüye yönlendir
+        SceneManager.LoadScene(0);
+        //oyuncu listesini güncelle
+        ChangePlayerList();
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour 
+{        
+    [SerializeField] GameObject player;
+    [SerializeField][Range(0.5f, 2f)]
+    float mouseSense = 1; 
+    [SerializeField][Range(-20, -10)]
+     int lookUp = -15;
+    [SerializeField][Range(15, 25)]
+    int lookDown = 20;
+    Animator anim;
+
+    public bool isSpectator;
+    //serbest kamera hareket modu hızı
+    [SerializeField] float speed = 50f;
+
+    private void Start() 
+    {
+        Cursor.lockState = CursorLockMode.Locked; 
+    }
+    void Update()
+    {
+        float rotateX = Input.GetAxis("Mouse X") * mouseSense;
+        float rotateY = Input.GetAxis("Mouse Y") * mouseSense;
+        if (!isSpectator)
+        {
+
+            Vector3 rotCamera = transform.rotation.eulerAngles;
+            Vector3 rotPlayer = player.transform.rotation.eulerAngles;
+
+            rotCamera.x = (rotCamera.x > 180) ? rotCamera.x - 360 : rotCamera.x;
+            rotCamera.x = Mathf.Clamp(rotCamera.x, lookUp, lookDown);
+            rotCamera.x -= rotateY;
+
+            rotCamera.z = 0;
+            rotPlayer.y += rotateX;
+
+            transform.rotation = Quaternion.Euler(rotCamera);
+            player.transform.rotation = Quaternion.Euler(rotPlayer);
+        }
+        else
+        {
+            //serbest kamera kodu 
+            //mevcut kamera açısını alalım
+            Vector3 rotCamera = transform.rotation.eulerAngles;
+
+            //farenin hareketine bağlı olarak kameranın açısını değiştirecez
+            rotCamera.x -= rotateY;
+            rotCamera.z = 0;
+            rotCamera.y += rotateX;
+
+            transform.rotation = Quaternion.Euler(rotCamera);
+
+            //wasd tuşlarını çekelim
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            //wasd tuşlarına göre kameranın konumunu ayarlama yani position
+            Vector3 dir = transform.right * x + transform.forward * z;
+
+            transform.position += dir * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            //eğer imleç kitliyse
+            if(Cursor.lockState == CursorLockMode.Locked) 
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            //eğer kitli değilse
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+
+ 
+    }
+}
+ 
